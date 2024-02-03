95621233a1a8bf4b73d0f8a80f3119e95eb152d0
kamendrovskiy <kamendrovskiy2006@mail.ru>
2024-02-03 15:53:35 +0300

    task 1 updated

--- task1.py
+++ task1.py
@@ -4,12 +4,17 @@ with open('scientist.txt', encoding = 'utf8') as file:
     f = list(csv.reader(file, delimiter = '#', quotechar = '"'))
     f = f[1:]
     c = {}
+    names = []
+    dates = []
     for s in f:
         date_list = s[2].split(sep = '-')
         date = int(date_list[0])*365 + int(date_list[1])*12 + int(date_list[2])
         date_old = c.get(s[1], 0)
         if date_old == 0 or date < date_old:
             c[s[1]] = date
+        if 'Аллопуринол'in s[1]:
+            names.append(s[0])
+            dates.append(s[2])
     for s in f:
         date_list = s[2].split(sep = '-')
         date = int(date_list[0])*365 + int(date_list[1])*12 + int(date_list[2])
@@ -21,3 +26,13 @@ with open('scientist_origin.txt', 'w', encoding = 'utf8', newline = '') as file:
     w = csv.writer(file, delimiter = '#', quotechar = '"')
     w.writerow(['ScientistName', 'preparation', 'date', 'components'])
     w.writerows(f)
+print('Разработчиками Аллопуринола были такие люди:')
+for i in range(len(names)):
+    print(names[i], ' - ', dates[i])
+for i in range(len(names)):
+    date = dates[i]
+    date_list = date.split(sep = '-')
+    date = int(date_list[0])*365 + int(date_list[1])*12 + int(date_list[2])
+    if date == c['Аллопуринол']:
+        print('Оригинальный рецепт принадлежит: ', names[i])
+    
